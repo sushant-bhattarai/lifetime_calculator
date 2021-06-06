fed26e80cdff19690d52153052ed272c38fb2b0b
Sushant Bhattarai <sushantb2055@gmail.com>
2021-06-06 19:16:53 +0545

    final commit fixing bugs

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function life() {
-    var year = document.getElementById("years");
-    var month = document.getElementById("months");
-    var day = document.getElementById("days");
-    var hour = document.getElementById("hours");
-    var min = document.getElementById("mins");
-    var second = document.getElementById("seconds");
-    var millisecond = document.getElementById("milliseconds");
+    var year = document.getElementById("years")
+    var month = document.getElementById("months")
+    var day = document.getElementById("days")
+    var hour = document.getElementById("hours")
+    var min = document.getElementById("mins")
+    var second = document.getElementById("seconds")
+    var millisecond = document.getElementById("milliseconds")
 
 
     date_value = localStorage["date_value"]
@@ -17,92 +17,102 @@ function life() {
 
     now = new Date()
     then = new Date(date_value)
-    month_ms_now = 0
-    month_ms_then = 0
+    // month_ms_now = 0
+    // month_ms_then = 0
 
 
-    mn = now.getMonth()+1
-    mt = then.getMonth()+1
+    // mn = now.getMonth()+1
+    // mt = then.getMonth()+1
 
-    if(mn==1 || mn==3 || mn==5 || mn==7 || mn==8 || mn==10 || mn==12){
-        month_ms_now = mn * 31*24*3600000
-    }
-    if(mt==1 || mt==3 || mt==5 || mt==7 || mt==8 || mt==10 || mt==12){
-        month_ms_then = mt * 31*24*3600000
-    }
-    if(mn==4 || mn==6 || mn==9 || mn==11){
-        month_ms_now = mn * 30*24*3600000
-    }
-    if(mt==4 || mt==6 || mt==9 || mt==11){
-        month_ms_then = mt * 30*24*3600000
-    }
-    if(mn == 2){
-        now_year = now.getFullYear();
-        if(isLeapYear(now_year)){
-            month_ms_now = mn * 29*24*3600000
-        }else{
-            month_ms_now = mn * 28*24*3600000
-        }
-    }
-    if(mt == 2){
-        then_year = then.getFullYear();
-        if(isLeapYear(then_year)){
-            month_ms_then = mt * 29*24*3600000
-        }else{
-            month_ms_then = mt * 28*24*3600000
-        }
-    }
+    // if(mn==1 || mn==3 || mn==5 || mn==7 || mn==8 || mn==10 || mn==12){
+    //     month_ms_now = mn * 31*24*3600000
+    // }
+    // if(mt==1 || mt==3 || mt==5 || mt==7 || mt==8 || mt==10 || mt==12){
+    //     month_ms_then = mt * 31*24*3600000
+    // }
+    // if(mn==4 || mn==6 || mn==9 || mn==11){
+    //     month_ms_now = mn * 30*24*3600000
+    // }
+    // if(mt==4 || mt==6 || mt==9 || mt==11){
+    //     month_ms_then = mt * 30*24*3600000
+    // }
+    // if(mn == 2){
+    //     now_year = now.getFullYear()
+    //     if(isLeapYear(now_year)){
+    //         month_ms_now = mn * 29*24*3600000
+    //     }else{
+    //         month_ms_now = mn * 28*24*3600000
+    //     }
+    // }
+    // if(mt == 2){
+    //     then_year = then.getFullYear()
+    //     if(isLeapYear(then_year)){
+    //         month_ms_then = mt * 29*24*3600000
+    //     }else{
+    //         month_ms_then = mt * 28*24*3600000
+    //     }
+    // }
 
-    // 1, 3, 5, 7, 8, 10, 12 - 31
-    //  4, 6, 9, 11 - 30
-    // 2 - 29 or 28
-    
-    console.log(month_ms_now - month_ms_then)
-
-    time1 = now.getFullYear()*365*24*3600000 + month_ms_now + now.getDate()*24*3600000 + now.getHours()*3600000 + now.getMinutes()*60000 + (now.getSeconds())*1000 + now.getMilliseconds();
-    time2 = then.getFullYear()*365*24*3600000 + month_ms_then + then.getDate()*24*3600000 + then.getHours()*3600000 + then.getMinutes()*60000 + then.getSeconds()*1000 + then.getMilliseconds();
-
-    milliseconds = time1 - time2;
-
-    actual_years = (milliseconds / (12*(365/12)*24*60*60*1000));
-    years = parseInt(actual_years);
-    rem_years = actual_years - years;
-    milliseconds = rem_years * 12*(365/12)*24*60*60*1000;
-    
-    actual_months = (milliseconds / ((365/12)*24*60*60*1000));
-    months = parseInt(actual_months);
-    rem_months = actual_months - months;
-    milliseconds = rem_months * (365/12)*24*60*60*1000;
-
-    actual_days = (milliseconds / (24*60*60*1000));
-    days = parseInt(actual_days);
-    rem_days = actual_days - days;
-    milliseconds = rem_days * 24*60*60*1000;
-
-    actual_hours = (milliseconds / (60*60*1000));
-    hours = parseInt(actual_hours);
-    rem_hours = actual_hours - hours;
-    milliseconds = rem_hours * 60*60*1000;
-
-    actual_mins = (milliseconds / (60*1000));
-    mins = parseInt(actual_mins);
-    rem_mins = actual_mins - mins;
-    milliseconds = rem_mins * 60*1000;
-
-    actual_seconds = (milliseconds / 1000);
-    seconds = parseInt(actual_seconds);
-    rem_seconds = actual_seconds - seconds;
-    milliseconds = parseInt(rem_seconds * 1000);
+    // // 1, 3, 5, 7, 8, 10, 12 - 31
+    // //  4, 6, 9, 11 - 30
+    // // 2 - 29 or 28
     
 
+    time1 = now.getFullYear()*365*24*3600000 + now.getMonth()*(365/12)*24*60*60*1000 
+            + now.getDate()*24*3600000 + now.getHours()*3600000 + now.getMinutes()*60000 
+            + (now.getSeconds())*1000 + now.getMilliseconds()
+    time2 = then.getFullYear()*365*24*3600000 + then.getMonth()*(365/12)*24*60*60*1000 
+            + then.getDate()*24*3600000 + then.getHours()*3600000 
+            + then.getMinutes()*60000 + then.getSeconds()*1000 + then.getMilliseconds()
 
-    years = years < 10 ? "0" + years : years;
-    months = months < 10 ? "0" + months : months;
-    days = days < 10 ? "0" + days : days;
+    milliseconds = time1 - time2
 
-    hours = hours < 10 ? "0" + hours : hours;
-    mins = mins < 10 ? "0" + mins : mins;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+    if(now.getMonth() == then.getMonth()+1){
+        milliseconds += 50400000
+    }
+    
+    // console.log(milliseconds)
+    
+
+    actual_years = (milliseconds / (12*(365/12)*24*60*60*1000))
+    years = parseInt(actual_years)
+    rem_years = actual_years - years
+    milliseconds = rem_years * 12*(365/12)*24*60*60*1000
+    
+    actual_months = (milliseconds / ((365/12)*24*60*60*1000))
+    months = parseInt(actual_months)
+    rem_months = actual_months - months
+    milliseconds = rem_months * (365/12)*24*60*60*1000
+
+    actual_days = (milliseconds / (24*60*60*1000))
+    days = parseInt(actual_days)
+    rem_days = actual_days - days
+    milliseconds = rem_days * 24*60*60*1000
+
+    actual_hours = (milliseconds / (60*60*1000))
+    hours = parseInt(actual_hours)
+    rem_hours = actual_hours - hours
+    milliseconds = rem_hours * 60*60*1000
+
+    actual_mins = (milliseconds / (60*1000))
+    mins = parseInt(actual_mins)
+    rem_mins = actual_mins - mins
+    milliseconds = rem_mins * 60*1000
+
+    actual_seconds = (milliseconds / 1000)
+    seconds = parseInt(actual_seconds)
+    rem_seconds = actual_seconds - seconds
+    milliseconds = parseInt(rem_seconds * 1000)
+    
+
+
+    years = years < 10 ? "0" + years : years
+    months = months < 10 ? "0" + months : months
+    days = days < 10 ? "0" + days : days
+
+    hours = hours < 10 ? "0" + hours : hours
+    mins = mins < 10 ? "0" + mins : mins
+    seconds = seconds < 10 ? "0" + seconds : seconds
 
     milliseconds = milliseconds.toString()
     if(milliseconds.length == 1){
@@ -110,17 +120,17 @@ function life() {
     }else if(milliseconds.length == 2){
         milliseconds = "0" + milliseconds
     }else{
-        milliseconds = milliseconds;
+        milliseconds = milliseconds
     }
 
 
-    year.innerHTML = years;
-    month.innerHTML = months;
-    day.innerHTML = days;
-    hour.innerHTML = hours;
-    min.innerHTML = mins;
-    second.innerHTML = seconds;
-    millisecond.innerHTML = milliseconds;
+    year.innerHTML = years
+    month.innerHTML = months
+    day.innerHTML = days
+    hour.innerHTML = hours
+    min.innerHTML = mins
+    second.innerHTML = seconds
+    millisecond.innerHTML = milliseconds
 
 }
 
--- form.js
+++ form.js
@@ -1,15 +1,32 @@
 function getMyDateTime(){
     myDate = document.getElementById("myDate").value;
     myTime = document.getElementById("myTime").value;
-    myDateTime = myDate.toString() + " " + myTime.toString();
+    myDateTime = myDate + " " + myTime + ":00:00";
+    now = new Date()
+    then = new Date(myDateTime)
 
-    d1 = new Date(myDateTime)
-    d2 = new Date()
 
-    if(d1 < d2){ //valid date input
+    if(date_validator(then, now)){ //valid date input
         localStorage["date_value"] = myDateTime;
         window.location.replace("index.html");
     }else{
         document.getElementById("error").innerHTML = "Invalid birth details!"
     }
+}
+
+function date_validator(then, now){
+    
+    time1 = now.getFullYear()*365*24*3600000 + now.getMonth()*(365/12)*24*60*60*1000 
+            + now.getDate()*24*3600000 + now.getHours()*3600000 
+            + now.getMinutes()*60000 + now.getSeconds()*1000 + now.getMilliseconds()
+    time2 = then.getFullYear()*365*24*3600000 + then.getMonth()*(365/12)*24*60*60*1000 
+            + then.getDate()*24*3600000 + then.getHours()*3600000 
+            + then.getMinutes()*60000 + then.getSeconds()*1000 + then.getMilliseconds()
+
+    if(time1 > time2){
+        return true
+    }else{
+        return false
+    }
+
 }
